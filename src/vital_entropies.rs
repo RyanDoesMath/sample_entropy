b5ab0f14096fb7e292932f44c6bb3dbea2ca9312
folksgl <Gfolks14@gmail.com>
2023-06-17 15:50:10 -0400

    Use Serde with CSV crates to write CSV file
    Instead of hand-writing the CSV file, which is prone to break if the
    VitalEntropies struct ever changes, we can automatically derive how the
    struct should be serialized to disk as csv data using Serde and CSV.
    This should result in more performant, less error-prone CSV handling.

--- src/vital_entropies.rs
+++ src/vital_entropies.rs
@@ -1,4 +1,7 @@
+use serde::{Deserialize, Serialize};
+
 /// Struct to store the name along with the entropy values.
+#[derive(Debug, Serialize, Deserialize)]
 pub struct VitalEntropies {
     pub name: String,
     pub sbp_sampen: f32,
